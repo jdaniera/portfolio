c413f90d4c50dd1506a5907c1e123273aba3d2d9
jdaniera <jasminejasmine14@hotmail.com>
2025-03-07 10:25:06 -0800

    added light vers logo, fixed project link in nav bar

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -1,20 +1,31 @@
 import Menu from "../Menu";
+import Image from "next/image";
 import styles from "./Intro.module.css";
 
 export default function Intro() {
-  return (
-    <div className={styles.introOuterContainer}>
-      <div className={styles.introInnerContainer}>
-        <h1>
-          Hello,
-        </h1>
-        <h1>
-            nice to meet you.
-        </h1>
-        <p>Jasmine Putri is a Vancouver-based multidisciplinary digital designer with a love for story-telling and human-centered design.
-        </p>
-      </div>
-      <Menu layout="column" />
-    </div>
-  );
+	return (
+		<div className={styles.introOuterContainer}>
+			<div className={styles.introInnerContainer}>
+				<h1>
+					Hello,
+					<br />
+					nice to meet you.
+				</h1>
+				<p>
+					Jasmine Putri is a Vancouver-based multidisciplinary digital designer
+					with a love for story-telling and human-centered design.
+				</p>
+			</div>
+			<div className={styles.logoContainer}> 
+				<Image
+					src="/images/logo/logo-light.svg"
+					alt="Jasmine Putri logo"
+					width={1920}
+					height={1080}
+					style={{ width: "100%", height: "auto" }}
+				/>
+			</div>
+			<Menu layout="column" />
+		</div>
+	);
 }
--- components/Menu/index.js
+++ components/Menu/index.js
@@ -2,17 +2,21 @@ import Link from "next/link";
 import styles from "./Menu.module.css";
 
 export default function Menu({ layout = "row" }) {
-    return (
-        <nav className={`${styles.navLinks} ${layout === "column" ? styles.column : styles.row}`}>
-            <Link href="#projectList" passHref>
-                <button>/ view projects</button>
-            </Link>
-            <Link href="/about" passHref>
-                <button>/ about me</button>
-            </Link>
-            <Link href="/contact" passHref>
-                <button className={styles.hideOnMobile}>/ contact</button>
-            </Link>
-        </nav>
-    );
+	return (
+		<nav
+			className={`${styles.navLinks} ${
+				layout === "column" ? styles.column : styles.row
+			}`}
+		>
+			<Link href="/#projectList" passHref>
+				<button>/ view projects</button>
+			</Link>
+			<Link href="/about" passHref>
+				<button>/ about me</button>
+			</Link>
+			<Link href="/contact" passHref>
+				<button className={styles.hideOnMobile}>/ contact</button>
+			</Link>
+		</nav>
+	);
 }
